0aaca44cf9ace97601ffb22bf7088662c42b27bc
cristiobal <cristiobal@hotmail.com>
2025-02-13 10:15:18 +0100

    Python Scripts creation

---
+++ Cristobal_MA/ejercicio5.py
@@ -0,0 +1,7 @@
+#Autor: Cris Moreno
+#Version: 1.0
+#Escribir un programa que convierta un valor dado en grados Fahrenheit a grados Celsius. Recordar que la fórmula para la conversión es
+
+grados=float(input("Dime una temperatura en Fahrenheit: "))
+celsius=((grados-32)*(5/9))
+print("Eso son",celsius,"grados celsius")
