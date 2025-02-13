975469cccdbb8c1e13e0cd4f6a8cfca216284422
Mohamed6977 <mchachi2005@gmail.com>
2025-02-13 12:43:23 +0100

    Delete Phyton directory

--- Phyton/Ejercicio18.py
+++
@@ -1,9 +0,0 @@
-# Autor: Mohamed Mchachi
-# Versión: 1.0
-# Enunciado:  Ejercicio 18: Pedir el nombre y los dos apellidos de una persona y mostrar las iniciales.
-
-Nombre = float(input("Ingresa el nombre: "))
-Apellido = float(input("Ingresa el apellido: "))
-Apellido2 = float(input("Ingresa el segundo apellido: "))
-
-print(f"Las iniciales de su nombre son {Nombre[0]}{Apellido[0]}{Apellido2[0]}")
