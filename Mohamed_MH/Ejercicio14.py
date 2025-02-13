d73ce781dfb7ed651e1190e02725969dabf964e1
Mohamed6977 <mchachi2005@gmail.com>
2025-02-13 12:45:02 +0100

    Ejercicios Python
    Ejercicios

---
+++ Mohamed_MH/Ejercicio14.py
@@ -0,0 +1,14 @@
+# Autor: Mohamed Mchachi
+# Versión: 1.0
+# Enunciado:  Ejercicio 14: Dado un número de dos cifras, diseñe un algoritmo que permita obtener el número invertido. Ejemplo, si se introduce 23 que muestre 32.
+
+
+numero = input("Ingresa un número: ")
+
+
+numero_invertido = numero[::-1]
+
+
+print(f"El número invertido es: {numero_invertido}")
+
+
---
+++ Mohamed_MH/Ejercicio4.py
@@ -0,0 +1,18 @@
+# Autor: Mohamed Mchachi
+# Versión: 1.0
+# Enunciado: Ejercicio 4: Dados dos números, mostrar la suma, resta, división y multiplicación de ambos.
+
+
+
+Numero1 = float(input("Ingresa el primer numero: "))
+Numero2 = float(input("Ingresa el segundo numero: "))
+
+suma = Numero1 + Numero2
+resta = Numero1 - Numero2
+multiplicacion = Numero1 * Numero2
+division = Numero1 / Numero2
+
+print(f"La suma es: {suma}")
+print(f"La resta es: {resta}")
+print(f"La multiplciacion es: {multiplicacion}")
+print(f"La division es: {division}")
